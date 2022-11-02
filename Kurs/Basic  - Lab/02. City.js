ae211b41146e5c944a1910a2b57220855cd484b3
dbozic15 <102158914+dbozic15@users.noreply.github.com>
2022-11-02 19:04:15 +0200

    add

---
+++ Kurs/Basic  - Lab/02. City.js
@@ -0,0 +1,7 @@
+function cityInfo(obj) {
+    for (let key of Object.keys(obj)) {
+        console.log(`${key} -> ${obj[key]}`)
+    }
+}
+
+cityInfo({ name: "Sofia", area: 492, population: 1238438, country: "Bulgaria", postCode: "1000" })
